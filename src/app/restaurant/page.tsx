bb7f8a7c0936777e4e12b2475099e8288883cf28
jeeeun <jek.1976534@gmail.com>
2024-09-29 18:51:59 +0900

    Add.

--- src/app/restaurant/page.tsx
+++ src/app/restaurant/page.tsx
@@ -1,20 +1,20 @@
-'use client';
-
 import List from '@/components/list/list';
 import RestaurantAddForm from './restaurant-add-form';
 import { fetchRestaurants } from '@/db/queries/restaurants';
 import { useSearchParams } from 'next/navigation';
+interface ShowPageProps {
+    searchParams: {
+        name: string;
+    };
+}
+export default function Page({ searchParams }: ShowPageProps) {
+    const { name } = searchParams;
+    console.log(searchParams);
 
-export default function Page() {
-    const searchParam = useSearchParams();
     return (
         <div className="container mx-auto p-4">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <List
-                    fetchData={() =>
-                        fetchRestaurants(searchParam.get('name') || '')
-                    }
-                />
+                <List fetchData={() => fetchRestaurants(name)} />
             </div>
             <RestaurantAddForm />
         </div>
